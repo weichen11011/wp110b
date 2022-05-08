74b55839b60d6f093365758fcf635195f89686e6
weichen11011 <99935264+weichen11011@users.noreply.github.com>
2022-05-08 15:11:31 +0800

    Create hw5-11.js

---
+++ hw5/hw5-11.js
@@ -0,0 +1,16 @@
+class ratio
+{
+    constructor(a,b)
+    {
+        this.a=a;
+        this.b=b;
+    }
+    toString()
+    {
+        return this.a+"/"+this.b;
+    }
+}
+var r1=new ratio(1,3);
+console.log(r1.toString());
+
+https://eyesofkids.gitbooks.io/javascript-start-from-es6/content/part4/prototype.html
