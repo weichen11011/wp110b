3dfcc8befd37e7a9ce86064d5020d5fc256af17b
weichen11011 <99935264+weichen11011@users.noreply.github.com>
2022-05-20 12:02:15 +0800

    Update vector.js

--- JS_homework/vector.js
+++ JS_homework/vector.js
@@ -1,58 +1,65 @@
 class vector {
-  
     constructor(array) {
-      this.a = array
+      this.x=array;
     }
   
     length() {
-      let b = 0;
-      for(let i = 0 ; i < this.a.length ; i++){
-        b += this.a[i]*this.a[i]
+      let a=0;
+      for (let i=0;i<this.x.length;i++)
+      {
+         a+=Math.pow(this.x[i],2);
       }
-      return Math.sqrt(b)
+      return Math.sqrt(a);
     }
   
     neg() {
-      for(let i = 0 ; i < this.a.length ; i++){
-        this.a[i] = -this.a[i]
+      let a=[]
+      for (let i=0;i<this.x.length;i++)
+      {
+        a[i]=this.x[i]*-1;
       }
-      return new vector(this.a)
+      return new vector(a);
     }
   
     add(p2) {
-      let b = [];
-      for(let i = 0 ; i < this.a.length ; i++){
-        b[i] = this.a[i] + p2.a[i]
+      let a=[];
+      for (let i=0;i<this.x.length;i++)
+      {
+        a[i]=this.x[i]+p2.x[i];
       }
-      return new vector(b)
+      return new vector(a);
     }
   
     sub(p2) {
       return this.add(p2.neg())
     }
   
-    distance(p2) {
-      return new vector(this.sub(p2).length())
+    dot(p2) {
+      let a=0;
+      for (let i=0;i<this.x.length;i++)
+      {
+        a+=this.x[i]*p2.x[i];
+      }
+      return a;
     }
   
-    dot(p2){
-      let b = 0;
-      for(let i = 0 ; i < this.a.length ; i++){
-        b += this.a[i]*p2.a[i]
+    distance(p2) {
+      let a=0;
+      for (let i=0;i<this.x.length;i++)
+      {
+        a+=Math.pow(this.x[i]-p2.x[i],2);
       }
-      return b
-    }
-
-    toString() {
-      return this.a.toString()
+      return Math.sqrt(a);
     }
   }
-  let p = new vector([2,3])
-  let p2 = new vector([1,2])
-  console.log('p.length()=', p.toString())
+  
+  let p = new vector([2,5]), p2 = new vector([1,3])
+  console.log('p=',p)
+  console.log('p2=',p2)
   console.log('p.length()=', p.length())
-  console.log('p.neg()=', p.neg())
-  console.log('p.add()=',p.add(p2))
-  console.log('p.sub()=',p.sub(p2))
-  console.log('p.distance()=',p.distance(p2))
-  console.log('p.dot()=',p.dot(p2))
+  console.log('p.neg,',p.neg())
+  console.log('p.add(p2),',p.add(p2))
+  console.log('p.sub(p2),',p.sub(p2))
+  console.log('p.dot(p2)',p.dot(p2))
+  console.log('p.distance(p2)',p.distance(p2))
+  
